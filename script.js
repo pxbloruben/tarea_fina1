2f94ec6fa392c1c105b3fa487f0670063a365fed
pxbloruben <rubenpablo33@gmail.com>
2025-03-06 21:38:06 -0600

    agrego las instrucciones para obtener los elementos del dom y los valores de entrada (comentarios y nombre de usuario)

---
+++ script.js
@@ -0,0 +1,8 @@
+// Obtener los elementos del DOM
+let agregar_button = document.getElementById("agregar_button");
+
+// Obtener los valores de los inputs del DOM usando su ID
+// En este caso es de los comentarios y el nombre del usuario
+let entry_input = document.getElementById("entry_input");
+let name_input = document.getElementById("name_input");
+
